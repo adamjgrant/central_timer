2a54468841f696f4d836b6ba5a40e3b7f511cc6c
Adam Grant <hello@adamgrant.me>
2022-07-27 14:55:32 -0700

    Stubbing out part where we create keys and shit

--- js/components/redis/redis.actions.js
+++ js/components/redis/redis.actions.js
@@ -9,15 +9,26 @@ m.redis.acts({
         return record.fields['value'];
     },
     async set_key(_$, args) {
-        let record = await m.airtable.act.set_by_value({
-            column_name: "key",
-            // Confusing. This specifies which value under the key column it should look for.
-            // For this column, each value under the "key" column is a key.
-            value: args.key,
-            fields: {
-                "value": args.value
-            }
-        });
-        return record.fields['value'];
+        let value = await _$.act.get_key(args);
+        if (value) {
+            return await m.airtable.act.set_by_value({
+                column_name: "key",
+                // Confusing. This specifies which value under the key column it should look for.
+                // For this column, each value under the "key" column is a key.
+                value: args.key,
+                fields: {
+                    "value": args.value
+                }
+            });
+        }
+        else {
+            await _$.act.create_key(args)
+        }
+    },
+
+    priv: {
+        async create_key(_$, args) {
+            // TODO
+        }
     }
 });
