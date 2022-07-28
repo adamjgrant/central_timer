60caabffc484621be8edefce0dffbd7382f57633
Adam Grant <hello@adamgrant.me>
2022-07-27 17:26:26 -0700

    Gentle persistence of timer sets

--- js/components/timer_set_library/timer_set_library.actions.js
+++ js/components/timer_set_library/timer_set_library.actions.js
@@ -1,10 +1,12 @@
 const REDIS_NAMESPACE = "central_timer_";
 
+m.timer_set_library.data = [];
 m.timer_set_library.acts({
   async get_all_timer_sets(_$, args) {
     const timer_sets = await m.redis.act.get_hash_key({
        key: `${REDIS_NAMESPACE}timer_sets`
     });
+    m.timer_set_library.data = timer_sets;
     return timer_sets;
   }
 });
